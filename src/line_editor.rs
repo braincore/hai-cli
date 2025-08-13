26ca1c3eab3e70d25490f93281a2f8c75c382bc9
Ken Elkabany <ken@elkabany.com>
2025-08-13 17:10:58 +0200

    HEP 10: Add auto-complete for /chat-resume & /asset-listen.

--- src/line_editor.rs
+++ src/line_editor.rs
@@ -594,6 +594,8 @@ impl Completer for CmdAndFileCompleter {
                 || line.starts_with("/asset-list ")
                 || line.starts_with("/ls ")
                 || line.starts_with("/asset-md-get ")
+                || line.starts_with("/asset-listen ")
+                || line.starts_with("/chat-resume ")
             {
                 let (cmd_word, arg_prefix, arg_index) = split_cmd_and_args(line);
                 if self.debug {
