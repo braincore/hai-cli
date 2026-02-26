217c642d03800c87734ffdd0a0ba2966d790082f
Ken Elkabany <ken@elkabany.com>
2026-02-26 11:59:12 -0800

    HEP 10/40: gateway: Don't set `s` if localhost.
    Makes URL cleaner.
    
    Also:
    
    - Don't add `#` fragment delimiter to URL if no fragments.
    - Fix debug mode + using localhost + public asset.

--- src/feature/asset_app.rs
+++ src/feature/asset_app.rs
@@ -31,54 +31,60 @@ pub async fn launch_browser(
         .await
     {
         let localhost_addr = format!("localhost:{}", ws_addr.port());
-        let (asset_prog_url, asset_prog_from_localhost) = if !debug
-            && let Some(asset_prog_url) = asset_helper::get_public_asset_url(&prog_asset_name)
-        {
-            (asset_prog_url, false)
-        } else {
-            // Prog-asset is private -> use proxy through the gateaway
-            use crate::api::types::asset::AssetGetArg;
-            match api_client
-                .asset_get(AssetGetArg {
-                    name: prog_asset_name.clone(),
-                })
-                .await
-            {
-                Ok(res) => {
-                    // NOTE: Both the encrypted and unencrypted code paths
-                    // use the same gateway proxy URL. They are kept
-                    // separate to make it clear that the encrypted path
-                    // has no other option but to proxy whereas the
-                    // unencrypted path could use the temporary public URL.
-                    if res
-                        .entry
-                        .metadata
-                        .as_ref()
-                        .map_or(false, |md| md.content_encrypted.is_some())
-                    {
-                        (
-                            format!("http://{}/~/{}", localhost_addr, prog_asset_name),
-                            true,
-                        )
-                    } else if let Some(_data_url) = res.entry.asset.url {
-                        (
-                            format!("http://{}/~/{}", localhost_addr, prog_asset_name),
-                            true,
-                        )
-                    } else {
-                        eprintln!(
-                            "error: asset '{}' does not have a link; cannot launch in browser",
-                            prog_asset_name
-                        );
+        let (asset_prog_url, asset_prog_from_localhost) =
+            if let Some(asset_prog_url) = asset_helper::get_public_asset_url(&prog_asset_name) {
+                if debug {
+                    (
+                        format!("http://{}{}", localhost_addr, prog_asset_name),
+                        true,
+                    )
+                } else {
+                    (asset_prog_url, false)
+                }
+            } else {
+                // Prog-asset is private -> use proxy through the gateaway
+                use crate::api::types::asset::AssetGetArg;
+                match api_client
+                    .asset_get(AssetGetArg {
+                        name: prog_asset_name.clone(),
+                    })
+                    .await
+                {
+                    Ok(res) => {
+                        // NOTE: Both the encrypted and unencrypted code paths
+                        // use the same gateway proxy URL. They are kept
+                        // separate to make it clear that the encrypted path
+                        // has no other option but to proxy whereas the
+                        // unencrypted path could use the temporary public URL.
+                        if res
+                            .entry
+                            .metadata
+                            .as_ref()
+                            .map_or(false, |md| md.content_encrypted.is_some())
+                        {
+                            (
+                                format!("http://{}/~/{}", localhost_addr, prog_asset_name),
+                                true,
+                            )
+                        } else if let Some(_data_url) = res.entry.asset.url {
+                            (
+                                format!("http://{}/~/{}", localhost_addr, prog_asset_name),
+                                true,
+                            )
+                        } else {
+                            eprintln!(
+                                "error: asset '{}' does not have a link; cannot launch in browser",
+                                prog_asset_name
+                            );
+                            return;
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("error: {}", e);
                         return;
                     }
                 }
-                Err(e) => {
-                    eprintln!("error: {}", e);
-                    return;
-                }
-            }
-        };
+            };
 
         let gateway_localhost_addr = format!("localhost:{}", ws_addr.port());
 
@@ -86,18 +92,21 @@ pub async fn launch_browser(
         if let Some(ref target) = target_asset_name {
             fragment_params.push(("asset", target));
         }
-        fragment_params.push(("s", &gateway_localhost_addr));
         if !asset_prog_from_localhost {
             fragment_params.push(("token", &auth_token));
+            fragment_params.push(("s", &gateway_localhost_addr));
         }
 
-        let fragment = fragment_params
-            .iter()
-            .map(|(k, v)| format!("{}={}", k, v))
-            .collect::<Vec<_>>()
-            .join("&");
-
-        let final_url = format!("{}#{}", asset_prog_url, fragment);
+        let final_url = if fragment_params.is_empty() {
+            asset_prog_url.to_string()
+        } else {
+            let fragment = fragment_params
+                .iter()
+                .map(|(k, v)| format!("{}={}", k, v))
+                .collect::<Vec<_>>()
+                .join("&");
+            format!("{}#{}", asset_prog_url, fragment)
+        };
         println!("Opening asset-as-editor in browser: {}", final_url);
         if let Err(e) = open::that_detached(final_url) {
             eprintln!("error: failed to open asset-as-editor in browser: {}", e);
